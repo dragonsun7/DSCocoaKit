4bcd165eb971cd6c9f5d32b5033439b01f832506
dragonsun7 <dragonsun7@163.com>
2017-12-26 22:38:34 +0800

    对 DSImageLabel 的属性撰写了更详尽的说明

--- DSCocoaKit/Views/Cells/DSImageLabelCell/DSImageLabelCell.h
+++ DSCocoaKit/Views/Cells/DSImageLabelCell/DSImageLabelCell.h
@@ -14,12 +14,43 @@
 
 + (CGFloat)defaultHeight;
 
-@property (nonatomic, strong) UIImage *image;                                   // 图片(如果传空，则不忽略图片显示)
-@property (nonatomic, copy) NSString *text;                                     // 文字
-@property (nonatomic, strong) UIFont *textFont;                                 // 文字字体(默认：系统14)
-@property (nonatomic, strong) UIColor *textColor;                               // 文字颜色(默认：grayColor)
-@property (nonatomic, assign) CGFloat imageLeftSpacing;                         // 图片的左间隔(默认：15.0)
-@property (nonatomic, assign) CGFloat textLeftSpacing;                          // 文字的左间隔(默认：8.0)
-@property (nonatomic, assign) DSCellSeparatorInsetStyle separatorInsetStyle;    // 分割线样式(默认：DSCellSeparatorInsetStyleDefault)
+/**
+ 要显示的图片，单元格内垂直居中
+ 显示的大小取决于图片的大小，如果为 nil，则忽略图片的显示
+ */
+@property (nonatomic, strong) UIImage *image;
+
+/**
+ 要显示的文字，单元格内垂直居中
+ */
+@property (nonatomic, copy) NSString *text;
+
+/**
+ 文字的字体，默认为系统字体，大小14.0
+ */
+@property (nonatomic, strong) UIFont *textFont;
+
+/**
+ 文字的颜色，默认为grayColor
+ */
+@property (nonatomic, strong) UIColor *textColor;
+
+/**
+ 图片距离单元格左边框的距离，默认为15.0
+ 当图片为 nil 时，该距离依然生效
+ */
+@property (nonatomic, assign) CGFloat imageLeftSpacing;
+
+/**
+ 文字起始位置与图片右边界的距离，默认为8.0
+ 当图片为 nil 时，该距离失效
+ */
+@property (nonatomic, assign) CGFloat textLeftSpacing;
+
+/**
+ 单元格分割线的样式，默认为 DSCellSeparatorInsetStyleDefault
+ 详情请参看 DSCellSeparatorInsetStyle 枚举的说明
+ */
+@property (nonatomic, assign) DSCellSeparatorInsetStyle separatorInsetStyle;
 
 @end
